f1a474f94658b342f949c91bc40c417788e80511
TLKing19 <kingoffl00t@gmail.com>
2026-04-19 15:45:44 +0600

    reap

--- scenes/menu/parallax_background.gd
+++ scenes/menu/parallax_background.gd
@@ -1,6 +1,9 @@
 extends ParallaxBackground
 
-@export var speed = 100
+@export var speed: float = 250.0          # скорость "поезда"
+var accumulated_offset: float = 0.0
 
-func _process(delta):
-	scroll_offset.x -= speed * delta
+func _process(delta: float) -> void:
+	accumulated_offset -= speed * delta
+	
+	scroll_base_offset.x = accumulated_offset
--- scenes/paralaxes/parallax_background_3.gd
+++ scenes/paralaxes/parallax_background_3.gd
@@ -6,6 +6,5 @@ var accumulated_offset: float = 0.0
 func _process(delta: float) -> void:
 	accumulated_offset -= speed * delta
 	
-	# Самое важное изменение:
 	scroll_base_offset.x = accumulated_offset
-	# НЕ используй scroll_offset, когда камера движется!
+	
