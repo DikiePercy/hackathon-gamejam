df6a11fd6b675f2695a3ee1d10c8fe81f63cb74e
Dastan Medetbekov <dastanmedetbekovv@gmail.com>
2026-04-19 11:55:16 +0600

    added music

--- scenes/train_timur/train.gd
+++ scenes/train_timur/train.gd
@@ -1,17 +1,22 @@
 extends Node2D
 
 @onready var wagons_container = $Wagons
+@onready var dst_train_engine_audio: AudioStreamPlayer = $TrainEngine
+@onready var dst_train_horn_audio: AudioStreamPlayer = $TrainHorn
 @export var wagon_width: float = 240.0
 @export var wagon_scene: PackedScene = preload("res://scenes/wagon/wagon.tscn")
 
 var speed = 200.0
-var wagons = [] 
+var wagons = []
 var is_in_depot = false
 
 func _ready():
 	# При старте уровня строим поезд по данным из GameManager
 	if !is_in_depot:
 		build_train_from_data()
+		_play_train_horn()
+		if speed > 0.0 and dst_train_engine_audio != null:
+			dst_train_engine_audio.play()
 
 func build_train_from_data():
 	# Очищаем, если что-то было
@@ -43,3 +48,7 @@ func build_train_from_data():
 
 func update_wagon_list():
 	wagons = wagons_container.get_children()
+
+func _play_train_horn() -> void:
+	if dst_train_horn_audio != null:
+		dst_train_horn_audio.play()
